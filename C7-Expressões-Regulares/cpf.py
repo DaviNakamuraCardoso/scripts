f83205bf5051c5760a4e07339656bdef3f2fd0df
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-09-20 22:33:13 -0300

    Updating cpf.py
    Refactoring the cpf.py code.

--- C7-Expressões-Regulares/cpf.py
+++ C7-Expressões-Regulares/cpf.py
@@ -1,12 +1,37 @@
-import re, pyperclip, sys
-
-phone_number = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-mo = phone_number.search('meu número é 415-555-4242')
-
-texto = pyperclip.paste()
-cpf_regex = re.compile(r'\d{9}-\d{2}')
-cpfs = cpf_regex.findall(texto)
-the_cpfs = '\n'.join(cpfs)
-pyperclip.copy(the_cpfs)
+import re
+import pyperclip 
+import sys 
 
 
+def main():
+
+    # Getting the text from the clipboard
+    text = pyperclip.paste()
+    
+    # Getting all the CPFs
+    cpfs = get_cpfs(text=text)
+
+    # Finally, the CPFs all copied to the clipboard
+    pyperclip.copy('\n'.join(cpfs))
+
+    return 
+
+
+def get_cpfs(text):
+    """
+    :param text: The text from which you want to extract the CPF numbers
+    :return: A list with all the CPFs in the given text 
+    """
+    # The Regex to identify the CPF numbers 
+    cpf_regex = re.compile(r'\d{3}(.)?\d{3}(.)?\d{3}(-)?\d{2}')
+
+    # Finding all the CPFs 
+    cpfs = cpf_regex.findall(text)
+
+    # Returning the texts
+    return cpfs  
+
+
+if __name__ == '__main__':
+    main()
+     
