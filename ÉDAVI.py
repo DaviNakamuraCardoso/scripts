0c9217ebab0d3d7ddda5f2334f6cd0e187641887
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-09-20 23:25:21 -0300

    Refactoring ÉDAVI.py

--- ÉDAVI.py
+++ ÉDAVI.py
@@ -1,13 +1,35 @@
-
-i = 0
 import os
-os.chdir('../down')
-for i in range(10000):
-    filename = open('edavi%s' % i, 'a')
-    filename.close()
 
-for filename in os.listdir('../down'):
-    if filename.startswith('edavi'):
-        print('Deletando', filename)
-        os.unlink('../down/%s' % filename)
+
+def main():
+    os.chdir('../down')
+    create_files() 
+    #delete_files()
+    return 
+
+
+def create_files():
+    """
+    Generates 10000 files  
+    :return: void
+    """
+    for i in range(10000):
+        filename = open('edavi%s' % i, 'a')
+        filename.close()
+    return
+
+
+def delete_files():
+    """
+    Delete all the files from create_files 
+    """
+    for filename in os.listdir('../down'):
+        if filename.startswith('edavi'):
+            print('Deletando', filename)
+            os.unlink('../down/%s' % filename)
+    return 
+
+
+if __name__ == '__main__':
+    main()
 
